6cd32f528c7f036238eaba45f8c0130112526577
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-11-24 07:46:57 +0100

    chore: improve linting rules

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('setup', () => {
-  it('works', () => {
+describe("setup", () => {
+  it("works", () => {
     expect(true).toBe(true);
   });
 });
