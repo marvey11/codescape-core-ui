7c03c54058363da68e7da8ed7436653b63f28d9a
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-11-24 10:30:44 +0100

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'CodescapeCoreUI',
-      fileName: 'codescape-core-ui',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "CodescapeCoreUI",
+      fileName: "codescape-core-ui",
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
-})
+});
